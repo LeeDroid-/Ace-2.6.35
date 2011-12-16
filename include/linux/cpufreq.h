a47f188305eef18060359ff5c508dcba9e0af23e
LeeDrOiD <leedroid.lb@gmail.com>
2011-12-16 11:36:32 +0000

    Add Official SmartassV2 (erasmux)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -358,6 +358,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMOOTHASS)
 extern struct cpufreq_governor cpufreq_gov_smoothass;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smoothass)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass2)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_POWERSAVE)
 extern struct cpufreq_governor cpufreq_gov_powersave;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_powersave)
